64b3e6ca598dff324dbe21cf42d28c1ad10faf85
peterk <peterkudelas@gmail.com>
2025-04-08 14:30:00 +0200

    initial setup

---
+++ app/components/hero.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="bg-background relative grid h-1/2 w-full grid-cols-1 grid-rows-1 rounded-lg border">
+    <ClientOnly>
+      <FlickeringGrid
+        class="relative inset-0 z-0"
+        :square-size="4"
+        :grid-gap="4"
+        color="#60A5FA"
+        :max-opacity="0.2"
+        :flicker-chance="0.6"
+      />
+    </ClientOnly>
+    <div class="absolute inset-0 flex flex-col items-center justify-center">
+      <div class="h-60">
+        <ParticleImage
+          image-src="/AboutMeNoBg.avif"
+          :mouse-force="'15'"
+          class="h-full"
+          canvas-height="400"
+        />
+      </div>
+    </div>
+  </div>
+</template>
