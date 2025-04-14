3db30214ff092b2dd3a423da61f2abdd759c1f1b
peterkudelas@gmail.com <peterkudelas@gmail.com>
2025-04-15 00:36:59 +0200

    start middleware

--- server/api/messages.get.ts
+++ server/api/messages.get.ts
@@ -1,4 +1,11 @@
-export default defineEventHandler(async () => {
+export default defineEventHandler(async event => {
+  if (!event.context.auth) {
+    throw createError({
+      status: 403,
+      message: 'You are not authorized my man',
+    })
+  }
+
   const messages = await useDrizzle().select().from(tables.messages).all()
   return messages
 })
